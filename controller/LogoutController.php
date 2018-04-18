9cd796e8bf0a36d0aee4c6f1f663073627a51db5
Jerico Lua <jericoluislua@yahoo.com.ph>
2018-04-18 11:30:36 +0200

    logout added

---
+++ controller/LogoutController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bluaje
+ * Date: 18.04.2018
+ * Time: 11:27
+ */
+class LogoutController
+{
+
+    public function index()
+    {
+
+    }
+}
--- view/header.php
+++ view/header.php
@@ -50,6 +50,7 @@
                 </li>
                 <li><a href="/login">Login</a></li>
                 <li><a href="/registration">Registration</a></li>
+                <li><a href="/logout">Logout</a></li>
             </ul>
         </div><!--/.nav-collapse -->
       </div>
