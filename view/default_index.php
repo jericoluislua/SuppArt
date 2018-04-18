872eb43d4f1f0efb3172194be852b61c6fc8c2e7
bchand <denis.chanmong@gmail.com>
2018-04-18 13:41:30 +0200

    Header Anpassung, Nav gelöscht

--- view/default_index.php
+++ view/default_index.php
@@ -1,4 +1,10 @@
 <div class="mid-section">
     <h1 id="headings"><?= $heading ?></h1>
 </div>
+<div class="content-title">
+    <h2 id="subtitles"></h2>
+</div>
+<div class="content">
 
+    <img src="images/post1.jpg" width="280" height="280" title="post1" alt="post1 />
+</div>
