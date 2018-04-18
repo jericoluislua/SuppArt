7db7c3059ecbdbfffc89571548be67a2106b9ff9
Lua Jerico Luis <bluaje@bbc-be.local>
2018-04-18 09:29:52 +0200

    Registration works

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bluaje
+ * Date: 18.04.2018
+ * Time: 09:17
+ */
+
+return array(
+    // Datebankkonfiguration
+    'database' => array(
+        'host'     => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'ArtDB',
+        ),
+    );
