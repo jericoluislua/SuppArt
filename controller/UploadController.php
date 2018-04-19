ed8cd4829bc82c3cdcbb75b7cac1dfe71aa9a1b5
bchand <denis.chanmong@gmail.com>
2018-04-19 11:02:52 +0200

    Created user_upload uploadController

---
+++ controller/UploadController.php
@@ -0,0 +1,10 @@
+<?php
+
+class uploadController{
+    public function index()
+    {
+
+    }
+}
+
+?>
---
+++ view/user_upload.php
@@ -0,0 +1,12 @@
+<div id="regform">
+
+<?php
+
+echo '<form enctype="multipart/form-data" action="upload.php" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="512000" />
+    Send this file: <input name="userfile" type="file" />
+    <input type="submit" value="Send File" />
+</form>';
+
+?>
+</div>
