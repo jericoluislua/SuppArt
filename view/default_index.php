82b664ad2018c7b9bc124819feab060b930c7608
Jerico Lua <jericoluislua@yahoo.com.ph>
2018-04-20 09:58:14 +0200

    ddd

--- view/default_index.php
+++ view/default_index.php
@@ -7,15 +7,19 @@
     <h4 class="subtitles2">Check out recent posts</h4>
 </div>
 <div class="content">
-    <p> <?php
+    <p>
+        <?php
 
         if(isset($_SESSION['LoggedIn'])) {
-            echo 'session started';
-        }else{
-            echo 'session not found';
+            echo 'Welcome ' . $_SESSION['LoggedIn'];
+
+        }
+        elseif(isset($loggedOut)){
+            echo 'Succesfully logged out. Welcome Guest.';
         }
 
-        ?></p>
+        ?>
+    </p>
     <?php include_once('post_index.php');?>
 
 </div>
