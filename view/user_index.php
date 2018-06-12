47423be007bbdb3103e505c1ac6b9bfdd02d1d71
jericoluislua <38457091+jericoluislua@users.noreply.github.com>
2018-06-12 20:46:58 +0200

    clean up

--- view/user_index.php
+++ view/user_index.php
@@ -1,19 +1,19 @@
 <article class="review open special">
     <?php if (isset($users)): ?>
-        -        <?php foreach ($users as $user): ?>
-            -            <div class="panel panel-default">
-                -                <div class="panel-heading"><?= $user->firstName; ?> <?= $user->lastName; ?></div>
-                -                <div class="panel-body">
-                    -                    <p class="description">  A user with the name <?= $user->firstName; ?> <?= $user->lastName; ?> already exists. User's E-Mail: <a href="mailto:<?= $user->email; ?>"><?= $user->email; ?></a></p>
-                    -                    <p>
-                        -                        <a title="Löschen" href="/user/delete?id=<?= $user->id ?>">Löschen</a>
-                        -                    </p>
-                    -                </div>
-                -            </div>
-            -        <?php endforeach ?>
-	<?php else: ?>
+                 <?php foreach ($users as $user): ?>
+                         <div class="panel panel-default">
+                                 <div class="panel-heading"><?= $user->firstName; ?> <?= $user->lastName; ?></div>
+                                 <div class="panel-body">
+                                         <p class="description">  A user with the name <?= $user->firstName; ?> <?= $user->lastName; ?> already exists. User's E-Mail: <a href="mailto:<?= $user->email; ?>"><?= $user->email; ?></a></p>
+                                         <p>
+                                                 <a title="Löschen" href="/user/delete?id=<?= $user->id ?>">Löschen</a>
+                                         </p>
+                                 </div>
+                         </div>
+                 <?php endforeach ?>
+    <?php else: ?>
         <div class="dhd">
             <h2 class="item title">Hoopla! Keine User gefunden.</h2>
         </div>
-	<?php endif ?>
+    <?php endif ?>
 </article>
